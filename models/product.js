2ae0a3525cdc59b73b56d9c63a02e05d3b36235b
HarikrishnanL <hari.hk1994@gmail.com>
2020-12-20 13:32:48 +0530

    model creating using temporary file storage

--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+// const products = [];
+const fs = require('fs');
+const path = require('path');
+
+
 
 module.exports = class Product {
     constructor(title) {
@@ -6,11 +10,34 @@ module.exports = class Product {
     };
 
     save(){
-        products.push(this);
+        // products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+        fs.readFile(p,(err,fileContent)=>{
+            let products = [];
+            console.log('file content for products',fileContent);
+            if(!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log('error while writing a file',err);
+            });
+        });
     };
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                console.log('error while fetching all products',err);
+                // return [];
+                cb([]);
+            }
+            // return JSON.parse(fileContent);
+            cb(JSON.parse(fileContent));
+        })
+        // return products;
     };
 
 };

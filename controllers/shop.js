9f93249ec991261f59745224b65d113dd06cae4e
HarikrishnanL <hari.hk19994@gmail.com>
2021-01-07 11:01:10 +0530

    adding pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,16 +7,33 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems ;
+
     Product.find()
+        .countDocuments()
+        .then(numProduct=>{
+            totalItems = numProduct;
+            return Product.find()
+                .skip((page-1)*ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then(products=>{
             res.render('shop/product-list',
                 {
                     prods:products,
                     pageTitle:"All Products",
-                    path:'/products'
+                    path:'/products',
+                    totalProducts:totalItems,
+                    currentPage:page,
+                    hasNextPage:ITEMS_PER_PAGE * page < totalItems,
+                    hasPreviousPage:page > 1,
+                    nextPage: page + 1 ,
+                    previousPage:page-1,
+                    lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
                 });
         })
         .catch(err=>{
@@ -25,6 +42,7 @@ exports.getProducts = (req, res, next) => {
             error.httpStatusCode = 500;
             return next(error);
         });
+
 };
 
 exports.getProduct = (req,res,next)=>{
@@ -51,13 +69,30 @@ exports.getProduct = (req,res,next)=>{
 };
 
 exports.getIndex = (req,res,next)=>{
+    const page = +req.query.page || 1;
+    let totalItems ;
+
     Product.find()
+        .countDocuments()
+        .then(numProduct=>{
+            totalItems = numProduct;
+            return Product.find()
+                .skip((page-1)*ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then(products=>{
             res.render('shop/index',
                 {
                     prods:products,
                     pageTitle:"Shop",
                     path:'/',
+                    totalProducts:totalItems,
+                    currentPage:page,
+                    hasNextPage:ITEMS_PER_PAGE * page < totalItems,
+                    hasPreviousPage:page > 1,
+                    nextPage: page + 1 ,
+                    previousPage:page-1,
+                    lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
                 });
         })
         .catch(err=>{
